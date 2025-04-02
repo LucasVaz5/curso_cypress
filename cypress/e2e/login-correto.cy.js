da92520a4476f83c45226de10ad266046f8f804e
Lucas Vaz <lucas.vs.br@gmail.com>
2025-04-02 18:34:23 -0300

    Adcionando novos testes automazidaos

--- cypress/e2e/login-correto.cy.js
+++ cypress/e2e/login-correto.cy.js
@@ -1,7 +1,10 @@
 describe("Página de login", () => {
-it ("Deve preencehr todos os campos do login corretamente e autenticar o usuário na pagina", () => {
-    cy.visit('https://adopet-frontend-cypress.vercel.app/');
-    cy.get('[data-test="login-button"]').click();
+    beforeEach(() => {
+        cy.visit('https://adopet-frontend-cypress.vercel.app/');
+        cy.get('[data-test="login-button"]').click();
+    })
+
+it ("Deve preencher todos os campos do login corretamente e autenticar o usuário na pagina", () => {
     cy.get('[data-test="input-loginEmail"]').type("tester@gmail.com");
     cy.get('[data-test="input-loginPassword"]').type("Teste123");
     cy.get('.pass__view').click();
