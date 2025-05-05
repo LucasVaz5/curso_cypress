ee1c1d3648389aa28e801a178dc69eb2a09f2b6e
Lucas Vaz <lucas.vs.br@gmail.com>
2025-05-05 19:57:59 -0300

    Mochawesome adicionado

--- cypress.config.js
+++ cypress.config.js
@@ -4,5 +4,14 @@ module.exports = {
       // implement node event listeners here
     },
     video:true,
+    reporter: 'mochawesome',
+
+    reporterOptions: {
+      reportDir: 'cypress/results',
+      overwrite: true,
+      html: true,
+      json: false,
+      timestamp: "mmddyyyy_HHMMss" },
+    
   },
 };
